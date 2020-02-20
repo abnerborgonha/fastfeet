f25946073c44bc82a02c2296d1cbbaf27c00c32f
guilhermekonell <gui.ri.konell@hotmail.com>
2020-02-20 13:31:04 -0300

    Update de usuário

--- backend/src/app/middlewares/auth.js
+++ backend/src/app/middlewares/auth.js
@@ -16,9 +16,9 @@ export default async (req, res, next) => {
     const decoded = await promisify(jwt.verify)(token, authConfig.secret);
 
     req.userId = decoded.id;
+
+    return next();
   } catch (error) {
     return res.status(401).json({ error: 'Token invalid.' });
   }
-
-  next();
 };
